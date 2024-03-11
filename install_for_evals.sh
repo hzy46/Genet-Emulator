a68e2a35aadc484c47467e5b953673e9e085509a
silver <silver@genet.myguest.virtualbox.org>
2024-03-11 13:55:29 -0500

    update install script

--- install_for_evals.sh
+++ install_for_evals.sh
@@ -12,10 +12,13 @@ sudo apt-get -yf install ./google-chrome-stable_current_amd64.deb
 pip3 install virtualenv
 
 virtualenv -p python3 tf_venv
-echo "$(pwd)/Genet-Emulator/src/emulator/abr" > tf_venv/lib/python3.6/site-packages/abr_emu.pth
+echo "$(pwd)/Genet-Emulator/src/emulator/abr" > tf_venv/lib/python3.8/site-packages/abr_emu.pth
 source tf_venv/bin/activate
 
 pip3 install numpy tensorflow==2.12.0 selenium pyvirtualdisplay numba torch tflearn xvfbwrapper matplotlib scipy
+pip3 install selenium==3.141.0
+pip3 install --upgrade urllib3==1.26.16
+pip3 install Pillow==9.5.0
 
 cd Genet-Emulator/src/emulator/abr/pensieve/virtual_browser/abr_browser_dir
 
