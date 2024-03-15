f8bc2f6d8bf58f86ff2a27958cd4c87809a42f47
Zhiyuan He <zhiyuhe@microsoft.com>
2024-03-15 16:27:39 +0800

    fix

--- src/emulator/abr/run_single_test.sh
+++ src/emulator/abr/run_single_test.sh
@@ -6,6 +6,6 @@ cd pensieve/video_server
 python video_server.py --port ${video_server_port} & 
 cd ../../
 
-mm-delay 40 mm-loss uplink 0 mm-loss downlink 0 mm-link pensieve/data/12mbps pensieve/data/starlink_scaled_mahimahi/starlink_trace_15.log -- bash -c "python -m pensieve.virtual_browser.virtual_browser --ip $\{MAHIMAHI_BASE\} --port ${video_server_port} --abr RL --video-size-file-dir pensieve/data/video_sizes --summary-dir pensieve/tests/GPT4_60_40_Starlink --trace-file starlink_trace_15.log --actor-path pensieve/data/mahimahi_new_best_models/gpt_eval_models/gpt4-starlink/nn_model_ep_4000.ckpt --abr-server-port=${abr_server_port}"
+mm-delay 40 mm-loss uplink 0 mm-loss downlink 0 mm-link pensieve/data/12mbps pensieve/data/starlink_scaled_mahimahi/starlink_trace_15.log -- bash -xc "python -m pensieve.virtual_browser.virtual_browser --ip $\{MAHIMAHI_BASE\} --port ${video_server_port} --abr RL --video-size-file-dir pensieve/data/video_sizes --summary-dir pensieve/tests/GPT4_60_40_Starlink --trace-file starlink_trace_15.log --actor-path pensieve/data/mahimahi_new_best_models/gpt_eval_models/gpt4-starlink/nn_model_ep_4000.ckpt --abr-server-port=${abr_server_port}"
 
 ps aux | grep ${video_server_port} | grep video_server | awk '{print $2}' | xargs kill
