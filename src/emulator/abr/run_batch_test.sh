96c6bb74e73487946e5b26814608e58e85c738c2
Zhiyuan He <zhiyuhe@microsoft.com>
2024-03-15 18:13:45 +0800

    fix

--- src/emulator/abr/run_batch_test.sh
+++ src/emulator/abr/run_batch_test.sh
@@ -22,10 +22,9 @@ port=31000
 trace_dir="pensieve/data/starlink_scaled_mahimahi/"
 trace_files=`ls ${trace_dir}`
 
-
-for method in default gpt35 gpt4; do
-    for trial in 0; do
-        for epoch in 4000; do
+for trial in 0 1 2 3 4; do
+    for epoch in 4000 3900 3800 3700 3600; do
+        for method in default gpt35 gpt4; do
             summary_dir="pensieve/tests/starlink/${method}-trial-${trial}-epoch-${epoch}"
             model_path="pensieve/data/mahimahi_new_best_models/all_starlink_eval_models/${method}-starlink/trial-${trial}/models/nn_model_ep_${epoch}.ckpt"
             for trace_file in ${trace_files} ; do
@@ -39,7 +38,7 @@ for method in default gpt35 gpt4; do
                 fi
                 echo $ret_file
                 bash run_single_test.sh ${port} ${mahimahi_link_file} ${summary_dir} ${trace_file} ${model_path} &> /tmp/log-${method}-trial-${trial}-epoch-${epoch}-trace-${trace_file} &
-                _lock_parallelism 20
+                _lock_parallelism 15
                 port=$((${port} + 5))
             done
         done
