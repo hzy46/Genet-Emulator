9d751c2fd6f8cb64c740d801827e5a5212d173c9
Zhiyuan He <zhiyuhe@microsoft.com>
2024-03-15 17:00:00 +0800

    fix

---
+++ src/emulator/abr/run_batch_test.sh
@@ -0,0 +1,42 @@
+set -x
+
+function _lock_parallelism {
+    parallelism_limit=$1
+    while true; do
+        current_parallelism=`jobs | grep Running | wc -l`
+        if [ "$current_parallelism" -ge "$parallelism_limit" ]; then
+            echo "Current parallelism is ${current_parallelism}, which is equal or larger than limit ${parallelism_limit}; sleep;"
+            sleep 20s
+        else
+            echo "Current parallelism is ${current_parallelism}, which is less than than limit ${parallelism_limit};"
+            break
+        fi
+    done
+} 
+
+
+# starlink test
+port=31000
+trace_dir="pensieve/data/starlink_scaled_mahimahi/"
+trace_files=`ls ${trace_dir}`
+
+
+
+
+for method in default gpt35 gpt4; do
+    for trial in 0; do
+        for epoch in 4000; do
+            summary_dir="pensieve/tests/starlink/${method}-trial-${trial}-epoch-${epoch}"
+            model_path="pensieve/data/all_starlink_eval_models/${method}-starlink/models/nn_model_ep_${epoch}.ckpt"
+            for trace_file in ${trace_files} ; do
+                mahimahi_link_file=${trace_dir}${trace_file}
+                bash run_single_test.sh ${port} ${mahimahi_link_file} ${summary_dir} ${trace_file} ${trace_file} &> /tmp/log-${method}-trial-${trial}-epoch-${epoch} &
+                _lock_parallelism 2
+                port=$((${port} + 5))
+            done
+        done
+    done
+done
+
+
+wait
--- src/emulator/abr/run_single_test.sh
+++ src/emulator/abr/run_single_test.sh
@@ -1,11 +1,29 @@
-base_port=15000
+# Example:
+# base_port: 15000
+# mahimahi_link_file: pensieve/data/starlink_scaled_mahimahi/starlink_trace_15.log
+# summary_dir: pensieve/tests/GPT4_60_40_Starlink
+# trace_file: starlink_trace_15.log
+# model_path: pensieve/data/mahimahi_new_best_models/gpt_eval_models/gpt4-starlink/nn_model_ep_4000.ckpt
+set -x
+
+base_port=$1
+mahimahi_link_file=$2
+summary_dir=$3
+trace_file=$4
+model_path=$5
+
+mkdir -p ${summary_dir}
+
 video_server_port=$((${base_port} + 1))
 abr_server_port=$((${base_port} + 2))
+
+ps aux | grep ${video_server_port} | grep video_server | awk '{print $2}' | xargs kill
+ps aux | grep ${abr_server_port} | grep virtual_browser | awk '{print $2}' | xargs kill
 
 cd pensieve/video_server
 python video_server.py --port ${video_server_port} & 
 cd ../../
 
-mm-delay 40 mm-loss uplink 0 mm-loss downlink 0 mm-link pensieve/data/12mbps pensieve/data/starlink_scaled_mahimahi/starlink_trace_15.log -- bash -xc "python -m pensieve.virtual_browser.virtual_browser --ip \${MAHIMAHI_BASE} --port ${video_server_port} --abr RL --video-size-file-dir pensieve/data/video_sizes --summary-dir pensieve/tests/GPT4_60_40_Starlink --trace-file starlink_trace_15.log --actor-path pensieve/data/mahimahi_new_best_models/gpt_eval_models/gpt4-starlink/nn_model_ep_4000.ckpt --abr-server-port=${abr_server_port}"
+mm-delay 40 mm-loss uplink 0 mm-loss downlink 0 mm-link pensieve/data/12mbps ${mahimahi_link_file} -- bash -xc "python -m pensieve.virtual_browser.virtual_browser --ip \${MAHIMAHI_BASE} --port ${video_server_port} --abr RL --video-size-file-dir pensieve/data/video_sizes --summary-dir ${summary_dir} --trace-file ${trace_file} --actor-path ${model_path} --abr-server-port=${abr_server_port}"
 
 ps aux | grep ${video_server_port} | grep video_server | awk '{print $2}' | xargs kill
