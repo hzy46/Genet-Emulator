0a1a21fd0fa7ab2ce037e8e226ed9996649a5c59
Zhiyuan He <zhiyuhe@microsoft.com>
2024-03-15 20:28:44 +0800

    fix

---
+++ src/emulator/abr/check_log_file.py
@@ -0,0 +1,15 @@
+import fire
+import pandas as pd
+import os
+
+def main(path):
+    if os.path.exists(path) is True:
+        try:
+            trace_df = pd.read_csv(path, delimiter="\t")
+            assert len(trace) == 49
+        except Exception as e:
+            print("will delete {}".format(path))
+            os.remove(path)
+
+if __name__ == '__main__':
+    main()
--- src/emulator/abr/run_batch_test.sh
+++ src/emulator/abr/run_batch_test.sh
@@ -17,34 +17,39 @@ function _lock_parallelism {
 # clear
 bash clear.sh
 
-# starlink test
-port=31000
-trace_dir="pensieve/data/starlink_scaled_mahimahi/"
-trace_files=`ls ${trace_dir}`
-
-for epoch in 4000 3900 3800 3700 3600; do
-    for trial in 0 1 2 3 4; do
-        for method in default gpt35 gpt4; do
-            summary_dir="pensieve/tests/starlink/${method}-trial-${trial}-epoch-${epoch}"
-            model_path="pensieve/data/mahimahi_new_best_models/all_starlink_eval_models/${method}-starlink/trial-${trial}/models/nn_model_ep_${epoch}.ckpt"
-            for trace_file in ${trace_files} ; do
-                mahimahi_link_file=${trace_dir}${trace_file}
-                ret_file=${summary_dir}"/log_RL_"${trace_file}
-                if [ -f ${ret_file} ]; then
-                    ret_file_size=$(stat -c%s "$ret_file")
-                    if [ "$ret_file_size" -ge "130" ]; then
-                        continue
+while [[ true ]]; do
+    # starlink test
+    port=31000
+    trace_dir="pensieve/data/starlink_scaled_mahimahi/"
+    trace_files=`ls ${trace_dir}`
+    exist_task=f
+    for epoch in 4000 3900 3800 3700 3600; do
+        for trial in 0 1 2 3 4; do
+            for method in default gpt35 gpt4; do
+                summary_dir="pensieve/tests/starlink/${method}-trial-${trial}-epoch-${epoch}"
+                model_path="pensieve/data/mahimahi_new_best_models/all_starlink_eval_models/${method}-starlink/trial-${trial}/models/nn_model_ep_${epoch}.ckpt"
+                for trace_file in ${trace_files} ; do
+                    mahimahi_link_file=${trace_dir}${trace_file}
+                    ret_file=${summary_dir}"/log_RL_"${trace_file}
+                    python check_log_file.py ${ret_file}  # delete if not valid
+                    if [ -f ${ret_file} ]; then
+                        contine
                     fi
-                fi
-                echo $ret_file
-                sleep 5s
-                bash run_single_test.sh ${port} ${mahimahi_link_file} ${summary_dir} ${trace_file} ${model_path} &> /tmp/log-${method}-trial-${trial}-epoch-${epoch}-trace-${trace_file} &
-                _lock_parallelism 40
-                port=$((${port} + 5))
+                    echo $ret_file
+                    sleep 5s
+                    bash run_single_test.sh ${port} ${mahimahi_link_file} ${summary_dir} ${trace_file} ${model_path} &> /tmp/log-${method}-trial-${trial}-epoch-${epoch}-trace-${trace_file} &
+                    exist_task=t
+                    _lock_parallelism 60
+                    port=$((${port} + 5))
+                done
             done
         done
     done
+    if [ $exist_task == "f" ]; then
+        break
+    fi
 done
+
 
 
 wait
